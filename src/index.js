1c611a13e97c6543db364e77ada5ecc8c5dc7c94
budgebi <budgebrian21@gmail.com>
2019-08-04 22:36:21 -0400

    Change color schme of board.

--- src/index.js
+++ src/index.js
@@ -215,8 +215,8 @@ class Chessboard {
     }
 
     drawBoard() {
-        const blackColor = '#8d6e63';
-        const whiteColor = '#E8E2C9';
+        const blackColor = '#546e7a';
+        const whiteColor = '#eeeeee';
 
         for (let r = 0; r < 8; r++) {
             for (let c = 0; c < 8; c++) {
